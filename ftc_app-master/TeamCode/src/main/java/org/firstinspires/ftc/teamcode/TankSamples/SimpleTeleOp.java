3964801f944e90b952dcdc8e39d7fa4cbd401b82
Ethan Schaffer <etanbet@gmail.com>
2017-08-15 22:07:20 -0500

    Add Samples for Tank Drive and Vuforia

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankSamples/SimpleTeleOp.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Ethan Schaffer on 7/3/2017.
+ */
+
+@TeleOp(name = "Simple Drive Tele")
+public class SimpleTeleOp extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor leftFront = hardwareMap.dcMotor.get(UniversalContants.leftFrontDrive);
+        DcMotor leftBack = hardwareMap.dcMotor.get(UniversalContants.leftBackDrive);
+        DcMotor rightFront = hardwareMap.dcMotor.get(UniversalContants.rightFrontDrive);
+        DcMotor rightBack = hardwareMap.dcMotor.get(UniversalContants.rightBackDrive);
+
+        SixWheelDrive drive = new SixWheelDrive(SimpleTeleOp.this, leftFront, leftBack, rightFront, rightBack);
+        waitForStart();
+        while(opModeIsActive()) {
+            gamepad1.setJoystickDeadzone(.1f);
+            gamepad2.setJoystickDeadzone(.1f);
+            waitForStart();
+
+            drive.setLeft(-Math.pow(gamepad1.left_stick_y, 3));
+            drive.setRight(-Math.pow(gamepad1.right_stick_y, 3));
+            // Cubing the powers scales to input a bit better.
+            // We can't square the input as then -1 would become +1
+        }
+    }
+
+}
