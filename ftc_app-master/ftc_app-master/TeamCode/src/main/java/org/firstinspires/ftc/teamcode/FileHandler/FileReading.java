1dac34e0785ac0544151f030be9ff43ccc6aa399
Ethan Schaffer <ethan@schaffer.com>
2017-09-17 10:01:30 -0500

    File Reading

---
+++ ftc_app-master/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileHandler/FileReading.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.FileHandler;
+
+import android.os.Environment;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class FileReading extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        String FILE_DIR = "/Download";
+        boolean lastAButton = gamepad1.a;
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + FILE_DIR, "route.txt");
+        Scanner s;
+        try {
+            s = new Scanner(file);
+            while(s.hasNext()){
+                if(gamepad1.a && !lastAButton){
+                    telemetry.addData("File Said:", s.next());
+                }
+                lastAButton = gamepad1.a;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
