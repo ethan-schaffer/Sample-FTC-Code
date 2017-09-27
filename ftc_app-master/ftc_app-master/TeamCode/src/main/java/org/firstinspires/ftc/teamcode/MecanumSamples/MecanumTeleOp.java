f96f917444da026af0e90d59a670b387fbdfa0b5
Ethan Schaffer <ethan@schaffer.com>
2017-09-26 20:10:56 -0500

    cleanup

--- ftc_app-master/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumSamples/MecanumTeleOp.java
+++ ftc_app-master/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumSamples/MecanumTeleOp.java
@@ -37,26 +37,6 @@ public class MecanumTeleOp extends OpMode {
         double inputX = Math.abs(gamepad1.left_stick_x) > ACCEPTINPUTTHRESHOLD ? -gamepad1.left_stick_x : 0;
         double inputC = Math.abs(gamepad1.right_stick_x)> ACCEPTINPUTTHRESHOLD ? -gamepad1.right_stick_x: 0;
 
-        double BIGGERTRIGGER = gamepad1.left_trigger > gamepad1.right_trigger ? gamepad1.left_trigger : gamepad1.right_trigger;
-        //Ternary, the larger trigger value is set to the value BIGGERTRIGGER
-
-        if(BIGGERTRIGGER > TRIGGERTHRESHOLD){ //If we have enough pressure on a trigger
-            if( (Math.abs(inputY) > Math.abs(inputX)) && (Math.abs(inputY) > Math.abs(inputC)) ){ //If our forwards motion is the largest motion vector
-                inputY /= 5*BIGGERTRIGGER; //slow down our power inputs
-                inputX /= 5*BIGGERTRIGGER; //slow down our power inputs
-                inputC /= 5*BIGGERTRIGGER; //slow down our power inputs
-            } else if( (Math.abs(inputC) > Math.abs(inputX)) && (Math.abs(inputC) > Math.abs(inputY)) ){ //and if our turing motion is the largest motion vector
-                inputY /= 4*BIGGERTRIGGER; //slow down our power inputs
-                inputX /= 4*BIGGERTRIGGER; //slow down our power inputs
-                inputC /= 4*BIGGERTRIGGER; //slow down our power inputs
-            } else if( (Math.abs(inputX) > Math.abs(inputY)) && (Math.abs(inputX) > Math.abs(inputC)) ){ //and if our strafing motion is the largest motion vector
-                inputY /= 3*BIGGERTRIGGER; //slow down our power inputs
-                inputX /= 3*BIGGERTRIGGER; //slow down our power inputs
-                inputC /= 3*BIGGERTRIGGER; //slow down our power inputs
-            }
-        }
-        //Use the larger trigger value to scale down the inputs.
-
         arcadeMecanum(inputY, inputX, inputC, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
     }
 
