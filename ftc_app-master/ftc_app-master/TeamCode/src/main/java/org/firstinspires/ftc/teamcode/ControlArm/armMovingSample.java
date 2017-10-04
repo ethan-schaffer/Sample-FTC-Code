fc9a74928b2b5494efcdccbbf118392d62382c9a
Ethan Schaffer <ethan@schaffer.com>
2017-10-04 10:56:06 -0500

    Arm Sample

---
+++ ftc_app-master/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControlArm/armMovingSample.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.ControlArm;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+public class armMovingSample extends LinearOpMode {
+    DcMotor arm;
+    TouchSensor touchSensorUp, touchSensorDown;
+    boolean armGoingUp = false, armGoingDown = false;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        arm = hardwareMap.dcMotor.get("arm");
+        touchSensorUp = hardwareMap.touchSensor.get("up");
+        touchSensorDown = hardwareMap.touchSensor.get("down");
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.y) {
+                //arm up!
+                armGoingUp = true;
+                armGoingDown = false;
+            } else if (gamepad1.a) {
+                //arm down!
+                armGoingDown = true;
+                armGoingUp = false;
+            }
+
+            if (armGoingUp && !touchSensorUp.isPressed()) {
+                // if our arm should be in the up position,
+                // and the touch sensor is not pressed,
+                // we should set the arm power up
+                arm.setPower(1);
+            } else if (armGoingDown && !touchSensorDown.isPressed()) {
+                // if our arm should be in the down position,
+                // and the touch sensor is not pressed,
+                // we should set the arm power down
+                arm.setPower(-1);
+            } else {
+                // if neither of these conditions are true,
+                // we should not set the power to anything
+                arm.setPower(0);
+            }
+        }
+    }
+}
