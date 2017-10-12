535b2bfbb0313cf2edb7e974b1cf669168b960d2
Ethan Schaffer <ethan@schaffer.com>
2017-10-12 11:05:49 -0500

    Document AutonomousWithMenu.java, remove comments from UniversalConstants.java

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMenuSample/AutonomousWithMenu.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMenuSample/AutonomousWithMenu.java
@@ -15,16 +15,21 @@ public class AutonomousWithMenu extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // initialize robot
         boolean lastDown = false, lastUp = false;
-        int waitTime = 1;
+        int waitTime = 0;
         while (!opModeIsActive()) {
             if (gamepad1.dpad_down && !lastDown) {
+                // We only want to increment if we pressed the button on this loop.
+                // If the button was pressed lastTime (lastDown is true) we don't want to add one more to waitTime.
+                // This would make waitTime go up very quickly.
                 waitTime++;
             } else if (gamepad1.dpad_up && !lastUp) {
                 if (waitTime > 0) {
+                    // If waitTime already equals 0, waitTime-- would make it negative.
+                    // We can't sleep for a negative amount of time, so we don't let the user make waitTime < 0;
                     waitTime--;
                 }
             }
-            telemetry.addData("Time Waiting: ", waitTime + " seconds");
+            telemetry.addData("Will wait for: ", waitTime + " seconds");
             telemetry.update();
             lastDown = gamepad1.dpad_down;
             lastUp = gamepad1.dpad_up;
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumSamples/UniversalConstants.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumSamples/UniversalConstants.java
@@ -9,9 +9,9 @@ If you use our code and see us at competition, come say hello!
 */
 
 public class UniversalConstants {
-    static final String LEFT1NAME = "l1"; //LX Port 2
-    static final String LEFT2NAME = "l2"; //LX Port 1
-    static final String RIGHT1NAME = "r1";//0A Port 1
-    static final String RIGHT2NAME = "r2";//0A Port 2
+    static final String LEFT1NAME = "l1";
+    static final String LEFT2NAME = "l2";
+    static final String RIGHT1NAME = "r1";
+    static final String RIGHT2NAME = "r2";
 
 }
